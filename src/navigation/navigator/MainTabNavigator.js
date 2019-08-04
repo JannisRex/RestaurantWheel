53ea3a0040788156e2a92e64f8b51eb18850eb6f
Jannis <rex.jannis@web.de>
2019-08-04 19:13:54 +0200

    [fixed] Icons not showing
    Signed-off-by: Jannis <rex.jannis@web.de>

--- src/navigation/navigator/MainTabNavigator.js
+++ src/navigation/navigator/MainTabNavigator.js
@@ -1,46 +1,40 @@
 import React from 'react'
 import { Platform } from 'react-native'
-import { createAppContainer, createStackNavigator, createBottomTabNavigator } from 'react-navigation'
+import { createAppContainer, createBottomTabNavigator } from 'react-navigation'
 import TabBarIcon from '../../lib/TabBarIcon'
-
 import SpinScreen from '../../screens/spin/main'
 import CustomSpinScreen from '../../screens/customSpin/main'
 
-const StackA = createStackNavigator({
-  SpinScreen: SpinScreen
+const BottomTabNavigator = createBottomTabNavigator({
+
+  ScreenA: {
+    screen: SpinScreen,
+    navigationOptions: {
+      header: null,
+      tabBarIcon: ({ focused }) => (
+        <TabBarIcon
+          class='Ionicons'
+          focused={focused}
+          name={
+            Platform.OS === 'ios'
+              ? `ios-information-circle${focused ? '' : '-outline'}`
+              : 'md-information-circle'} />
+      )
+    }
+  },
+
+  ScreenB: {
+    screen: CustomSpinScreen,
+    navigationOptions: {
+      header: null,
+      tabBarIcon: ({ focused }) => (
+        <TabBarIcon
+          class='Entypo'
+          focused={focused}
+          name='aircraft' />
+      )
+    }
+  }
 })
 
-StackA.navigationOptions = {
-  tabBarLabel: 'Help',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      class='Ionicons'
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  )
-}
-
-const StackB = createStackNavigator({
-  CustomSpinScreen: CustomSpinScreen
-})
-
-StackB.navigationOptions = {
-  tabBarLabel: 'inOut',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      class='Entypo'
-      focused={focused}
-      name='aircraft'
-    />
-  )
-}
-
-export default createAppContainer(createBottomTabNavigator({
-  SpinScreen,
-  CustomSpinScreen
-}))
+export default createAppContainer(BottomTabNavigator)
