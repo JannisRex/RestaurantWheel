ed877ce26746e7d14408b91dec28ad446d74a493
Jannis <rex.jannis@web.de>
2019-08-04 14:36:14 +0200

    [added] Tab Navigator for different visualisation types
    Signed-off-by: Jannis <rex.jannis@web.de>

---
+++ src/navigation/navigator/MainTabNavigator.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Platform } from 'react-native'
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation'
+import TabBarIcon from '../../lib/TabBarIcon'
+
+import SpinScreen from '../../screens/spin/main'
+import CustomSpinScreen from '../../screens/customSpin/main'
+
+const StackA = createStackNavigator({
+  SpinScreen: SpinScreen
+})
+
+StackA.navigationOptions = {
+  tabBarLabel: 'Help',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      class='Ionicons'
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  )
+}
+
+const StackB = createStackNavigator({
+  CustomSpinScreen: CustomSpinScreen
+})
+
+StackB.navigationOptions = {
+  tabBarLabel: 'inOut',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      class='Entypo'
+      focused={focused}
+      name='aircraft'
+    />
+  )
+}
+
+export default createBottomTabNavigator({
+  SpinScreen,
+  CustomSpinScreen
+})
