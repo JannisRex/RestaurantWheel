603a44bf6e7b9e37d62c558a03dd023b81ae4422
Jannis <rex.jannis@web.de>
2019-08-04 13:31:06 +0200

    [added] default Restaurant template
    Signed-off-by: Jannis <rex.jannis@web.de>

---
+++ src/assets/data/cologneRestaurants.js
@@ -0,0 +1,11 @@
+// https://places.factual.com
+export const cologneRestaurants = {
+  'Restaurant A': {
+    sector: '(Volle Bedienung) | (beschränktes Angebot - Imbiss) | (Bars) ...', // maybe category
+    food: 'Italian | Steak | American ... ',
+    times : {monday: [{12:00}, {18:00}], tuesday: []}  // opens at 12, closes at 18
+    address: 'Street',
+    phone: {0221 26130},
+    website: 'https://www.frueh.de/'
+  }
+}
