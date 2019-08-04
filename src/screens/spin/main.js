9ab40c336ab5a6206809f3f1c096b14c34298571
Jannis <rex.jannis@web.de>
2019-08-04 18:16:40 +0200

    [added] Slider props and styles
    Signed-off-by: Jannis <rex.jannis@web.de>

--- src/screens/spin/main.js
+++ src/screens/spin/main.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
-import { getRestaurant, Settings } from '../../components/index'
+import { GetRestaurant, Settings } from '../../components/index'
 import Swiper from 'react-native-swiper'
+import styles from './styles'
 
 
 class SpinScreen extends Component {
   render() {
     return (
-      <View style={{ flex: 1 }} >
+      <View style={styles.container}>
         <Swiper
-
-        />
-        <View>
-          <getRestaurant />
-        </View>
-        <View>
-          <Settings />
-        </View>
+          showsPagination={false}
+          index={0}>
+          <View style={styles.container}>
+            <GetRestaurant />
+          </View>
+          <View style={styles.container}>
+            <Settings />
+          </View>
+        </Swiper>
       </View>
     )
   }
