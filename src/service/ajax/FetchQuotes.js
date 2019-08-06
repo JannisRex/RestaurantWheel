1dec98486d7624c25d7362c0515eaa251a204cda
Jannis Rex <rex.jannis@web.de>
2019-08-06 12:28:20 +0200

    [changed] fetch url
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/service/ajax/FetchQuotes.js
+++ src/service/ajax/FetchQuotes.js
@@ -1,6 +1,6 @@
-const url = 'https://gist.github.com/JannisRex/17a6279edc7a191ca8e3ba563807b2d5'
+const url = 'http://157.230.31.202'
 
 export const FetchQuotes = () => {
-  return fetch(url + '/menuALL')
+  return fetch(url + '/quotesALL')
     .then(response => response.json())
 }
